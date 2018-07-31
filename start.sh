be5def05bd55107c3b9a340f2adf26a6f62d0bbc
Rodd Talebi <roddtalebi@gmail.com>
2018-07-31 09:42:15 -0700

    updated with kill commands

--- start.sh
+++ start.sh
@@ -4,3 +4,5 @@ sudo git fetch origin
 sudo git reset --hard origin/master
 cd /home/
 sudo python3 /home/kuber-master/worker.py
+ting=$(curl http://metadata.google.internal/computeMetadata/v1/instance/name -H "Metadata-Flavor: Google")
+gcloud compute instances delete $ting --zone=us-west1-b --delete-disks=all
