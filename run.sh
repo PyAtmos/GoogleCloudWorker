ae6a94bf0b058a35219db486a990f121a12e3e7f
William James Fawcett <william.fawcett@cern.ch>
2018-08-05 20:14:59 -0700

    store run output

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ export WORKERDIR=/code/kuber-master
 export PYATMOSDIR=/code/pyatmos
 echo "starting worker ..."
 echo $PWD
-/usr/bin/python3 /code/kuber-master/worker.py
+/usr/bin/python3 /code/kuber-master/worker.py &> /code/kuber-master/run_log.txt
 echo "completed worker"
 sleep 35000d 
